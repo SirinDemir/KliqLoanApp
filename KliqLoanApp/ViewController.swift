19a3bbd38463058d5746d4263af0278be48aed23
Yusuf Cinar <ycinar@bpn.com.tr>
2025-02-02 15:55:33 +0300

    Initial Commit

---
+++ KliqLoanApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KliqLoanApp
+//
+//  Created by Yusuf Çınar on 2.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
